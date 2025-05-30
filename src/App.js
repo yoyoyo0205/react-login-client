260680926256dc2a9bfaa9a8188584936fd248f7
yo <32672663+yoyoyo0205@users.noreply.github.com>
2025-05-30 21:10:38 +0900

    トークン取得とトークンを使ったAPI認証実装

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import { useState } from 'react';
 
 function App() {
   const [token, setToken] = useState('');
+  const [message, setMessage] = useState('');
 
   const login = async () => {
-    const response = await axios.post('http://localhost:8080/auth/login', {
-      username: 'airi',
-      password: 'password',
-    });
-    setToken(response.data.token);
-    console.log('取得したトークン:', response.data.token);
+    try {
+      const response = await axios.post('http://localhost:8080/auth/login', {
+        username: 'airi',
+        password: 'password',
+      });
+      setToken(response.data.token);
+      console.log('取得したトークン:', response.data.token);
+    } catch (error) {
+      console.log('ログイン失敗:', error);
+    }
+  };
+
+  const getHelloMessage = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/admin/hello', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setMessage(response.data);
+    } catch (error) {
+      console.error('hello API 呼び出し失敗:', error);
+      setMessage('API呼び出しに失敗しました。');
+    }
   };
 
   return (
@@ -18,6 +37,11 @@ function App() {
       <h1>JWT Login Test</h1>
       <button onClick={login}>ログイン</button>
       <p>Token: {token}</p>
+
+      <button onClick={getHelloMessage} disabled={!token}>
+        トークンで /auth/hello 呼び出し
+      </button>
+      <p>メッセージ：{message}</p>
     </div>
   );
 }
